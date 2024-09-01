1a65bedfec98ee96ab39edb92159741092217f49
Abdallah Abdelmoaty <abdallah.abdelmoaty@tasahel.local>
2024-09-01 15:44:32 +0300

    remove duplicates elements from sorted array

--- Array/Array.js
+++ Array/Array.js
@@ -1,6 +1,7 @@
 // Remove Duplicates from Sorted Array
 // 1- replace all duplicated elements with "_"
 // let nums = [0,0,1,1,1,2,2,3,3,4]; => [0,_,1,_,_,2,_,3,_,4]
+
 let nums = [0,0,1,1,1,2,2,3,3,4];
 let removeDuplicates = (nums)=>{
     let k = 0;
@@ -10,12 +11,25 @@ let removeDuplicates = (nums)=>{
             nums[i] = "_";
         }else{
             pointer = nums[i];
+            k++
         }
     }
-    
-    console.log(nums);
+    k++
+    let pointer2 = 1;
+    for (let d = 1; d < nums.length; d++) {
+        if(nums[d]==="_"){
+            for (let j = d+1; j < nums.length; j++) {
+                if(nums[j]!=="_"){
+                    nums[d] = nums[j];
+                    nums[j] = "_";
+                    break
+                }
+            }
+        }   
+    }
+    return k
     
 }
     
-removeDuplicates(nums)
+console.log(removeDuplicates(nums));
 
