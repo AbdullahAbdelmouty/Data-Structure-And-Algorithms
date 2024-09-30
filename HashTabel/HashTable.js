40ef8938a77161d4dfaee973abb20ae6e43f264c
AbdullahAbdelmouty <abdallah0115019@gmail.com>
2024-09-30 13:22:39 +0300

    Report Spam Message

---
+++ HashTabel/HashTable.js
@@ -0,0 +1,19 @@
+// Report Spam Message
+// const reportSpam = function(message,bannedWord){
+//     const bannedSet = new Set(bannedWord);
+//     let count = 0;
+//     for(const word of message){
+//         if(bannedSet.has(word)){
+//             count++;
+//             if(count===2){
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// const message = ["hello","programming","fun"];
+// const bannedWords = ["world","programming","leetcode"];
+
+// console.log(reportSpam(message,bannedWords));
