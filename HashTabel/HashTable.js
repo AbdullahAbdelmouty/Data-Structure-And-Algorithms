ffb4e77b37b70ae0b5674e49adb5240359eaf7fe
AbdullahAbdelmouty <abdallah0115019@gmail.com>
2024-10-03 15:49:11 +0300

    Longest Substring Without Repeating Characters

--- HashTabel/HashTable.js
+++ HashTabel/HashTable.js
@@ -17,3 +17,25 @@
 // const bannedWords = ["world","programming","leetcode"];
 
 // console.log(reportSpam(message,bannedWords));
+
+// Longest Substring Without Repeating Characters
+// const lengthOfLongestSubstring = function(s) {
+//     let right = 0;
+//     let left = 0;
+//     let maxLength = 0;
+//     let set = new Set();
+//     while(right<s.length) {
+//         if(set.has(s[right])){
+//             set.delete(s[left]);
+//             left++;
+//         }else{
+//             set.add(s[right]);
+//             maxLength = Math.max(maxLength, right - left + 1);
+//             right++;
+
+//         }
+//     }   
+//     return maxLength;
+// };
+// const str = "abcabcbb";
+// console.log(lengthOfLongestSubstring(str));
